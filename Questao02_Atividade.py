83a4d56e4969d86965a6207275c67ca32add4e4e
João Felipe <felipewgt56@gmail.com>
2020-08-18 20:10:35 -0300

    Criei o arquivo Questao02_Atividade.py

---
+++ Questao02_Atividade.py
@@ -0,0 +1,9 @@
+def retorno(a):
+    return ord(a)
+def main():
+    nome = input('Digite um único caractere para saber código numérico correspondente: ')
+    caractere = retorno(nome)
+    print(caractere)
+if __name__=='__main__':
+    main()
+    
