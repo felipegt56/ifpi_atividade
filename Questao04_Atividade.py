39812881a57ed3830133fbfdeeda0b488c18abd8
João Felipe <felipewgt56@gmail.com>
2020-08-19 09:11:24 -0300

    Atualizei o arquivo Questao04_Atividade.py

--- Questao04_Atividade.py
+++ Questao04_Atividade.py
@@ -1,5 +1,11 @@
-def num(n1, n2, n3, n4, n5, maior, menor, media):
-    return max(maior), min(menor)
+def maximo(n1,n2,n3,n4,n5):
+    return max(n1, n2 ,n3 ,n4 ,n5)
+
+def minimo(n1,n2,n3,n4,n5):
+    return min(n1, n2, n3, n4, n5)
+
+def media_5(n1, n2, n3, n4, n5):
+    return (n1 + n2 + n3 + n4 + n5) / 5
 
 def main():
     num1 = int(input('Digite o 1° número: '))
@@ -7,12 +13,12 @@ def main():
     num3 = int(input('Digite o 3° número: '))
     num4 = int(input('Digite o 4° número: '))
     num5 = int(input('Digite o 5° número: '))
-    maxi = [num1,num2,num3,num4,num5]
-    mini = [num1,num2,num3,num4,num5]
-    medi = (num1 + num2 + num3 + num4 + num5) / 5
-    print(f'O maior número lido foi {max(maxi)}')
-    print(f'O menor número lido foi {min(mini)}')
-    print(f'A média aritmética e {medi}')
+    resultado = media_5(num1,num2,num3,num4,num5)
+    maxi = maximo(num1,num2,num3,num4,num5)
+    mini = minimo(num1,num2,num3,num4,num5)
+    print(f'O maior número lido foi {maxi}')
+    print(f'O menor número lido foi {mini}')
+    print(f'A média aritmética e {resultado:.2f}')
 
 if __name__=='__main__':
     main()
