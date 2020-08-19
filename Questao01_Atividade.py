35113b3de236b56eb420326738367f6668be74d6
João Felipe <felipewgt56@gmail.com>
2020-08-19 06:17:00 -0300

    Atualizei o arquivo Questao01_Atividade.py

--- Questao01_Atividade.py
+++ Questao01_Atividade.py
@@ -1,8 +1,10 @@
 def nome(nome):
     return len(nome)
+
 def main():
     nom = str(input('Digite seu nome: ')).strip()
     nam = nome(nom)
     print(f' Seu nome possui {nam} letras.')
+
 if __name__=='__main__':
     main()
