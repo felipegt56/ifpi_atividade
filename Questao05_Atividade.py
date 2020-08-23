59a00c4c011c62dac0496132002af2cb7c82b0c2
João Felipe <felipewgt56@gmail.com>
2020-08-23 20:14:20 -0300

    Atualizei o arquivo Questao05_Atividade.py

--- Questao05_Atividade.py
+++ Questao05_Atividade.py
@@ -1,18 +1,20 @@
+def a_vista(p):
+    return p - (p * (9/100))
+
 def parcela_5(cinco_vz):
     return cinco_vz /5
 
 def parcela_10(dez_vz):
-    return dez_vz / 10
-
+    return (dez_vz + (dez_vz * (17/100))) / 10
+            
 def main():
     compra = float(input('Preço do Produto: R$ '))
-    vista = compra - (compra * (9/100))
-    dez_vz = compra +(compra * (17/100))
+    vista = a_vista(compra)
     pac_5 = parcela_5(compra)
-    pac_10 = parcela_10(dez_vz)
-    print(f'Pagamento à vista com desconto de 9%: R${vista:.2f} reais.')
-    print(f'Prestação para pagamento em 5 vezes sem juros: R${pac_5:.2f} reais.')
-    print(f'Prestação para pagamento em 10 vezes, com 17% de juros: R${pac_10:.2f} reais.')
+    pac_10 = parcela_10(compra)
+    print(f'Pagamento à vista com desconto de 9%: R${vista:0.2f} reais.')
+    print(f'Prestação para pagamento em 5 vezes sem juros: R${pac_5:0.2f} reais.')
+    print(f'Prestação para pagamento em 10 vezes, com 17% de juros: R${pac_10:0.2f} reais.')
 
 if __name__=='__main__':
     main()
