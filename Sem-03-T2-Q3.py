c7268a8a97618b1eddb05b14011f7bf2e0ef7b27
João Felipe <felipewgt56@gmail.com>
2020-08-31 00:23:31 -0300

    Criei o arquivo Sem-03-T2-Q3.py

---
+++ Sem-03-T2-Q3.py
@@ -0,0 +1,6 @@
+caractere = input('Digite um caractere qualquel: ').lower()
+if caractere in ('b','c','d','f','g','h','j','k','l','m','n','p','q','r','s','t','v','w','x','y','z'):
+  print(True)
+else:
+  print(False)
+  
