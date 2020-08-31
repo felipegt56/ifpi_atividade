28e4a5b32486e87691ad8572892d3f02fda3aa3b
João Felipe <felipewgt56@gmail.com>
2020-08-31 11:28:26 -0300

    Criei o arquivo Sem-03-T1-Q5.py

--- Sem-03-T1-Q5.py
+++ Sem-03-T1-Q5.py
@@ -1,9 +1,9 @@
 def media_f(a, b, c):
   return (a + b + c) / 3
 def main():
-  nota1 = int(input())
-  nota2 = int(input())
-  nota3 = int(input())
+  nota1 = int(input('Digite a Primeira nota: ))
+  nota2 = int(input('Digite a Segunda nota: ))
+  nota3 = int(input('Digite a Terceira nota: ))
   media = media_f(nota1,nota2,nota3)
   if nota3 > 8:
     media_final =  1
